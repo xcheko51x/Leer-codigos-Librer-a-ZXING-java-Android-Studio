c014877a08446d537e918f58d5d39ee07c10aa2d
xcheko51x <xcheko51x@gmail.com>
2022-12-15 15:54:20 -0600

    Ejemplo sencillo de implementación librería ZXING con Java y Android Studio

---
+++ app/src/main/java/com/example/leer_codigo_barras_zxing_java_2022/CaptureActivityPortrait.java
@@ -0,0 +1,6 @@
+package com.example.leer_codigo_barras_zxing_java_2022;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+}
---
+++ app/src/main/java/com/example/leer_codigo_barras_zxing_java_2022/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.leer_codigo_barras_zxing_java_2022;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.leer_codigo_barras_zxing_java_2022.databinding.ActivityMainBinding;
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() == null) {
+            Toast.makeText(this, "CANCELADO", Toast.LENGTH_SHORT).show();
+        } else {
+            binding.etCodigo.setText(result.getContents());
+        }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLeerCodigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                escanear();
+            }
+        });
+    }
+
+    public void escanear() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
+        options.setPrompt("ESCANEAR CODIGO");
+        options.setCameraId(0);
+        options.setOrientationLocked(false);
+        options.setBeepEnabled(false);
+        options.setCaptureActivity(CaptureActivityPortrait.class);
+        options.setBarcodeImageEnabled(false);
+
+        barcodeLauncher.launch(options);
+    }
+}
